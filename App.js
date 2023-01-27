5bfc69e686119b255f3f71f493ee85cbed493737
Adem Hatay <hatayadem5@gmail.com>
2023-01-27 22:42:18 +0300

    bg-red-400

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 
 export default function App() {
 	return (
-		<View className="flex-1 items-center justify-center bg-green-400">
+		<View className="flex-1 items-center justify-center bg-red-400">
 			<StatusBar style="auto" />
 			<Text>Open up App.js to start working on your app!</Text>
 		</View>
