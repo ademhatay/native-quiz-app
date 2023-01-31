b8b3d26edfaf8d3747d3ade40950dd23814506b3
Adem Hatay <hatayadem5@gmail.com>
2023-01-31 13:09:38 +0300

    colors changed

--- app/screens/QuizScreen.jsx
+++ app/screens/QuizScreen.jsx
@@ -19,45 +19,45 @@ const QuizScreen = () => {
 					</View>
 				</View>
 
-				{/* QustionsBar */}
+				{/* QuestionsBar */}
 				<View className="flex-1 flex-row justify-around items-center px-5">
 					<ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
 						<View className="flex-row">
 							<TouchableOpacity className="bg-primary px-5 py-3 rounded-lg mx-2">
 								<Text className="text-2xl text-white">1</Text>
 							</TouchableOpacity>
-							<TouchableOpacity className="bg-red-400 px-5 py-3 rounded-lg mx-2">
+							<TouchableOpacity className="bg-gray-100 px-5 py-3 rounded-lg mx-2">
 								<Text className="text-2xl">2</Text>
 							</TouchableOpacity>
-							<TouchableOpacity className="bg-red-400 px-5 py-3 rounded-lg mx-2">
+							<TouchableOpacity className="bg-gray-100 px-5 py-3 rounded-lg mx-2">
 								<Text className="text-2xl">3</Text>
 							</TouchableOpacity>
-							<TouchableOpacity className="bg-red-400 px-5 py-3 rounded-lg mx-2">
+							<TouchableOpacity className="bg-gray-100 px-5 py-3 rounded-lg mx-2">
 								<Text className="text-2xl">4</Text>
 							</TouchableOpacity>
-							<TouchableOpacity className="bg-red-400 px-5 py-3 rounded-lg mx-2">
+							<TouchableOpacity className="bg-gray-100 px-5 py-3 rounded-lg mx-2">
 								<Text className="text-2xl">5</Text>
 							</TouchableOpacity>
-							<TouchableOpacity className="bg-red-400 px-5 py-3 rounded-lg mx-2">
+							<TouchableOpacity className="bg-gray-100 px-5 py-3 rounded-lg mx-2">
 								<Text className="text-2xl">6</Text>
 							</TouchableOpacity>
-							<TouchableOpacity className="bg-red-400 px-5 py-3 rounded-lg mx-2">
+							<TouchableOpacity className="bg-gray-100 px-5 py-3 rounded-lg mx-2">
 								<Text className="text-2xl">7</Text>
 							</TouchableOpacity>
-							<TouchableOpacity className="bg-red-400 px-5 py-3 rounded-lg mx-2">
+							<TouchableOpacity className="bg-gray-100 px-5 py-3 rounded-lg mx-2">
 								<Text className="text-2xl">8</Text>
 							</TouchableOpacity>
-							<TouchableOpacity className="bg-red-400 px-5 py-3 rounded-lg mx-2">
+							<TouchableOpacity className="bg-gray-100 px-5 py-3 rounded-lg mx-2">
 								<Text className="text-2xl">9</Text>
 							</TouchableOpacity>
-							<TouchableOpacity className="bg-red-400 px-5 py-3 rounded-lg mx-2">
+							<TouchableOpacity className="bg-gray-100 px-5 py-3 rounded-lg mx-2">
 								<Text className="text-2xl">10</Text>
 							</TouchableOpacity>
 						</View>
 					</ScrollView>
 				</View>
 
-				{/* Qustion & options*/}
+				{/* Question & options*/}
 				<View className="flex-[3]">
 					<View className="px-5 py-3">
 						<Text className="text-2xl font-bold">
@@ -65,22 +65,22 @@ const QuizScreen = () => {
 						</Text>
 					</View>
 					<View className="items-center mt-6">
-						<TouchableOpacity className="w-10/12 bg-red-500 justify-center items-center rounded-lg py-4">
+						<TouchableOpacity className="w-10/12 bg-gray-200 justify-center items-center rounded-lg py-4">
 							<Text className="text-xl">
 								New Delhi
 							</Text>
 						</TouchableOpacity>
-						<TouchableOpacity className="w-10/12 bg-red-500 justify-center items-center rounded-lg py-4 mt-4">
+						<TouchableOpacity className="w-10/12 bg-gray-200 justify-center items-center rounded-lg py-4 mt-4">
 							<Text className="text-xl">
 								Mumbai
 							</Text>
 						</TouchableOpacity>
-						<TouchableOpacity className="w-10/12 bg-red-500 justify-center items-center rounded-lg py-4 mt-4">
+						<TouchableOpacity className="w-10/12 bg-gray-200 justify-center items-center rounded-lg py-4 mt-4">
 							<Text className="text-xl">
 								Kolkata
 							</Text>
 						</TouchableOpacity>
-						<TouchableOpacity className="w-10/12 bg-red-500 justify-center items-center rounded-lg py-4 mt-4">
+						<TouchableOpacity className="w-10/12 bg-gray-200 justify-center items-center rounded-lg py-4 mt-4">
 							<Text className="text-xl">
 								Chennai
 							</Text>
@@ -92,15 +92,15 @@ const QuizScreen = () => {
 				{/* Buttons */}
 				<View className="flex-1 flex-row justify-around items-center px-5">
 					<View className="flex-row flex-[2] justify-evenly">
-						<TouchableOpacity className="bg-red-400 px-5 py-3 rounded-lg items-center justify-center">
-							<Ionicons name="arrow-back" size={34} color="black" />
+						<TouchableOpacity className="bg-blue-200 px-5 py-3 rounded-lg items-center justify-center">
+							<Ionicons name="arrow-back" size={34} color="white" />
 						</TouchableOpacity>
-						<TouchableOpacity className="bg-red-400 px-5 py-3 rounded-lg items-center justify-center">
-							<Ionicons name="arrow-forward" size={34} color="black" />
+						<TouchableOpacity className="bg-blue-200 px-5 py-3 rounded-lg items-center justify-center">
+							<Ionicons name="arrow-forward" size={34} color="white" />
 						</TouchableOpacity>
 					</View>
 					<TouchableOpacity className="bg-primary px-5 py-3 rounded-lg">
-						<Text className="text-2xl">Submit</Text>
+						<Text className="text-2xl text-white">Submit</Text>
 					</TouchableOpacity>
 				</View>
 			</View>
